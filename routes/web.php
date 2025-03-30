e6c6af674e1089f54ea56488858359d8e581b816
Wrseno <warsenosetyono@gmail.com>
2025-03-30 08:54:45 +0700

    feat: add all routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PendudukController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')
+    ->name('admin.')
+    // ->middleware(['auth', 'admin'])
+    ->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+        Route::get('/penduduk/search', [PendudukController::class, 'search'])->name('penduduk.search');
+        Route::resource('penduduk', PendudukController::class)->parameters(['penduduk' => 'uuid']);
 });
