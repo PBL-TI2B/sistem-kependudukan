db31e581d7bfdea475b26558296d3c35b800bfed
Wrseno <warsenosetyono@gmail.com>
2025-04-06 12:44:44 +0700

    feat: add export pdf and excel route

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,13 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PendudukController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::prefix('admin')
     ->name('admin.')
     // ->middleware(['auth', 'admin'])
     ->group(function () {
         Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
         Route::get('/penduduk/search', [PendudukController::class, 'search'])->name('penduduk.search');
+        Route::get('/penduduk/export-pdf', [PendudukController::class, 'exportPdf'])->name('penduduk.export-pdf');
+        Route::get('/penduduk/export-excel', [PendudukController::class, 'exportExcel'])->name('penduduk.export-excel');
         Route::resource('penduduk', PendudukController::class)->parameters(['penduduk' => 'uuid']);
 });
