b3eabc6b4f1333b5657f3fb6a6e2e30572253a50
Rohimatun <rohimatunnurin13@gmail.com>
2025-04-09 22:44:21 +0700

    up

--- components/header.php
+++ components/header.php
@@ -12,7 +12,6 @@
 
   <!-- Navigation -->
   <nav class="items-center space-x-6 text-sm font-medium">
-<<<<<<< HEAD
     <a
       href="/beranda.php"
       class="<?= ($active == 'beranda') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>"
@@ -43,13 +42,5 @@
       class="bg-[#E5A025] text-black px-4 py-1 rounded-full hover:bg-[#d9981c] transition"
       >Masuk</a
     >
-=======
-    <a href="../beranda" class="<?= ($active == 'beranda') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>">Beranda</a>
-    <a href="../profilDesa" class="<?= ($active == 'profil') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>">Profil Desa</a>
-    <a href="../infografis" class="<?= ($active == 'infografis') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>">Infografis</a>
-    <a href="../berita" class="<?= ($active == 'berita') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>">Berita</a>
-    <a href="../galeri" class="<?= ($active == 'galeri') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>">Galeri</a>
-    <a href="/login.php" class="bg-[#E5A025] text-black px-4 py-1 rounded-full hover:bg-[#d9981c] transition">Masuk</a>
->>>>>>> 2fb2520f428c810c2d5d619e1c6323484fc29d27
   </nav>
 </header>
