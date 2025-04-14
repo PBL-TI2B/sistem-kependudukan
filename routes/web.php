f076320f0ec021108d1c1361c04f6d57112a1fc8
Wrseno <warsenosetyono@gmail.com>
2025-04-14 20:51:54 +0700

    feat: add new routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\PendudukController;
 
+use App\Http\Controllers\Admin\DomisiliController;
+
 Route::prefix('admin')
     ->name('admin.')
     // ->middleware(['auth', 'admin'])
@@ -13,4 +15,10 @@ Route::prefix('admin')
         Route::get('/penduduk/export-pdf', [PendudukController::class, 'exportPdf'])->name('penduduk.export-pdf');
         Route::get('/penduduk/export-excel', [PendudukController::class, 'exportExcel'])->name('penduduk.export-excel');
         Route::resource('penduduk', PendudukController::class)->parameters(['penduduk' => 'uuid']);
+
+        Route::get('/domisili/search', [DomisiliController::class, 'search'])->name('domisili.search');
+        Route::get('/domisili/export-pdf', [DomisiliController::class, 'exportPdf'])->name('domisili.export-pdf');
+        Route::get('/domisili/export-excel', [DomisiliController::class, 'exportExcel'])->name('domisili.export-excel');
+
+        Route::resource('domisili', DomisiliController::class)->parameters(['domisili' => 'uuid']);
 });
