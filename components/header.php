19f3f6f1ed486af818549d72aa720bf5a0c0c431
Rohimatun <rohimatunnurin13@gmail.com>
2025-04-29 01:36:57 +0700

    nurin login beranda navbar footer done

--- components/header.php
+++ components/header.php
@@ -1,4 +1,4 @@
-<header class="bg-[#0B391D] text-white px-4 md:px-8 py-3 flex flex-wrap justify-between items-center">
+<header class="bg-[#0B391D] text-white px-4 md:px-8 py-3 flex flex-wrap justify-between items-center sticky top-0 z-50">
   <!-- Logo -->
   <div class="flex items-center space-x-3">
     <img src="../../src/logo.png" alt="Logo Desa Jabung" class="h-16" />
@@ -20,16 +20,14 @@
   <nav id="menu" class="hidden w-full md:flex md:items-center md:space-x-6 text-sm font-medium md:w-auto mt-4 md:mt-0">
     <a href="../beranda"
       class="<?= ($active == 'beranda') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>">Beranda</a>
-    <a href="../profilDesa"
-      class="<?= ($active == 'profil') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>">Profil Desa</a>
     <a href="../infografis"
       class="<?= ($active == 'infografis') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>">Infografis</a>
     <a href="../berita"
       class="<?= ($active == 'berita') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>">Berita</a>
     <a href="../galeri"
       class="<?= ($active == 'galeri') ? 'text-[#F6C646]' : 'hover:text-[#F6C646]' ?>">Galeri</a>
-    <!--<a href="../login"
-      class="mt-3 md:mt-0 bg-[#E5A025] text-black px-4 py-1 rounded-full hover:bg-[#d9981c] transition">Masuk</a> -->
+    <a href="../login"
+      class="mt-3 md:mt-0 bg-[#E5A025] text-black px-4 py-1 rounded-full hover:bg-[#d9981c] transition">Masuk</a>
   </nav>
 
   <!-- Script for Toggle -->
